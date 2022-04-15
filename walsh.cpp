71d396bed07d6fd59e2a5573cc325f05eec0db6a
vladzinca <vlad_zinca@yahoo.com>
2022-04-15 18:12:55 +0300

    Successfully implements walsh.cpp

--- walsh.cpp
+++ walsh.cpp
@@ -1,41 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Task
-{
-public:
-    void solve()
-    {
+class Task {
+ public:
+    void solve() {
         read_input();
         print_output(get_result());
     }
 
-private:
+ private:
     int n, k;
     vector<int> x, y;
 
-    void read_input()
-    {
+    void read_input() {
         ifstream fin("walsh.in");
         fin >> n >> k;
         x.push_back(-1);
         y.push_back(-1);
-        for (int i = 1, e, f; i <= k; i++)
-        {
+        for (int i = 1, e, f; i <= k; i++) {
             fin >> e >> f;
             x.push_back(e);
-            y.push_back(e);
+            y.push_back(f);
         }
         fin.close();
     }
 
-    vector<int> get_result()
-    {
-        return x;
+    int negate(int x) {
+        if (x == 0)
+            return 1;
+        else
+            return 0;
     }
 
-    void print_output(vector<int> result)
-    {
+    int walsh(int x, int y, int n) {
+        if (n == 2) {
+            if (x == 2 && y == 2)
+                return 1;
+            else
+                return 0;
+        } else {
+            if (x <= n / 2 && y <= n / 2)
+                return walsh(x, y, n / 2);
+            else if (x <= n / 2 && y > n / 2)
+                return walsh(x, y - n / 2, n / 2);
+            else if (x > n / 2 && y <= n / 2)
+                return walsh(x - n / 2, y, n / 2);
+            else
+                return negate(walsh(x - n / 2, y - n / 2, n / 2));
+        }
+    }
+
+    vector<int> get_result() {
+        vector<int> v;
+        v.push_back(-1);
+        for (int i = 1; i <= k; i++)
+            v.push_back(walsh(x[i], y[i], n));
+        return v;
+    }
+
+    void print_output(vector<int> result) {
         ofstream fout("walsh.out");
         for (int i = 1; i <= k; i++)
             fout << result[i] << "\n";
@@ -43,11 +66,9 @@ private:
     }
 };
 
-int main()
-{
+int main() {
     auto *task = new (nothrow) Task();
-    if (!task)
-    {
+    if (!task) {
         cerr << "new failed: WTF are you doing? Throw your PC!\n";
         return -1;
     }
